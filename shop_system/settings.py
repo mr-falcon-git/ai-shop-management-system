7cd286895dd70623b3f84acde7485abaa7b5468d
Mr Falcon <mr.falcon.ab@gmail.com>
2026-05-14 00:36:20 -0700

    fix deployment

--- shop_system/settings.py
+++ shop_system/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = 'django-insecure-%cl5dz8vc%kznlr0-)5-7r6xs+*69v4hn1un84j-qog%pkfdd5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -66,7 +66,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
